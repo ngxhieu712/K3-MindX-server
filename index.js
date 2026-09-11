477806180ccc654538f0384163b236961115f32b
Nguyen Hiu <ngxhieu712@gmail.com>
2026-09-12 00:48:19 +0700

    đổi lại port 8080

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(cookieParser());
 
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const MONGODB_URI = process.env.MONGODB_URI;
 
 app.use('/api/auth', authRouter);
