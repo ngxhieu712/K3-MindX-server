75f276b8639ae4defb7546b290696d1cdfb88ec2
Nguyen Hiu <ngxhieu712@gmail.com>
2026-09-14 05:49:53 +0700

    mở cors cho client deploy

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ const app = express();
 const ALLOWED_ORIGINS = [
   process.env.CLIENT_ORIGIN || "http://localhost:5173",
   process.env.ADMIN_ORIGIN || "http://localhost:5174",
+  "https://k3-mind-x-adminweb.vercel.app",
+  "https://k3-mind-x-client.vercel.app"
 ];
 
 app.use(cors({
